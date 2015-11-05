66cf875db5f78b889a6275273f988a72f6d9e314
Will Simmons <willsimmons@gmail.com>
2015-11-04 20:32:56 -0800

    moving to gh pages

--- web.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var zippon = require('gzippo');
-var app = express();
-
-app.use('/static', express.static(__dirname + '/dist'));
-app.listen(process.env.PORT || 5000);
-app.get('/', function (req, res){
-  res.send "index.html";
-});
