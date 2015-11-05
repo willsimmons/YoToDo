542297de346f17a09085859978daa197193dd361
Will Simmons <willsimmons@gmail.com>
2015-11-04 19:03:59 -0800

    add and delete todos

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,10 +8,25 @@
  * Controller of the yotodoApp
  */
 angular.module('yotodoApp')
-  .controller('MainCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
+  .controller('MainCtrl', function($scope) {
+    $scope.todos = [
+      'Learn Angular',
+      'Learn Yeoman',
+      'Learn Grunt',
+      'Learn Bower'
     ];
+    $scope.newToDo = function() {
+    	// simple error handling
+    	if($scope.todo === ''){
+    		return;
+    	}
+    	if($scope.todos.indexOf($scope.todo) !== -1){
+    		return;
+    	}
+    	$scope.todos.push($scope.todo);
+    	$scope.todo = '';
+    };
+    $scope.removeToDo = function(index) {
+    	$scope.todos.splice(index,1);
+    };
   });
