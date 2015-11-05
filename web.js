1750132799ce22071de77af44171eec895ecab35
Will Simmons <willsimmons@gmail.com>
2015-11-04 20:29:27 -0800

    index fix

--- web.js
+++ web.js
@@ -5,5 +5,5 @@ var app = express();
 app.use('/static', express.static(__dirname + '/dist'));
 app.listen(process.env.PORT || 5000);
 app.get('/', function (req, res){
-  res.render "index.html";
+  res.send "index.html";
 });
